d85986933469a6d5a18ecfe4b53cc50f2cc307fa
veerababu <penkeyveerababu229@gmail.com>
2024-12-05 23:46:27 +0530

    added navbar responsive in smaller screens

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import underline from '../../assets/nav_underline.svg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import menu_open from '../../assets/menu_open.svg'
 import menu_close from '../../assets/menu_close.svg'
-import v2_logo from '../../assets/v2_logo.jpg'
 import v2_logo_tranparent from '../../assets/v2_logo_transparent.png'
 
 const Navbar = () => {
@@ -16,16 +15,16 @@ const Navbar = () => {
     menuRef.current.style.right = "0";
   }
   const closeMenu = () => {
-    menuRef.current.style.right = "-350px";
+    menuRef.current.style.right = "-375px";
   }
 
 
   return (
     <div className='navbar'>
       <img className='navbar-logo' src={v2_logo_tranparent} alt="" />
-      <img onClick={openMenu} src={menu_open}  alt=""  className='nav-mob-open' />
+      <img onClick={openMenu}  src={menu_open}  alt=""  className='nav-mob-open' /> 
       <ul className='nav-menu' ref={menuRef}>
-        <img onClick={closeMenu} src={menu_close} alt="" className='nav-mob-close' />
+        <img onClick={closeMenu } src={menu_close} alt="" className='nav-mob-close'/>
         <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu('home')}>Home</p></AnchorLink>{menu === 'home' ? <img src={underline} alt="" /> : <></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu('about')} >About Me</p></AnchorLink>{menu === 'about' ? <img src={underline} alt="" /> : <></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => setMenu('services')}>Services</p></AnchorLink>{menu === 'services' ? <img src={underline} alt="" /> : <></>}</li>
