c37ef0eee31f37b5e1a0238c98a9bb85f595471a
veerababu <penkeyveerababu229@gmail.com>
2025-12-24 19:17:55 +0530

    make changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Hero.css';
-import profile_img from '../../assets/veera2_mini.png'
+import profile_img from '../../assets/v2_change_mini.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Hero = () => {
   return (
@@ -10,7 +10,7 @@ const Hero = () => {
         <p>I am a fullstack developer from Andhra pradesh, India  </p>
         <div className="hero-action">
             <div className='hero-connect'> <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-            <a href="https://drive.google.com/file/d/1oGWJUki0S8Aq3bf5vaCq8IP3KkldX8e5/view?usp=drive_link"><div className="hero-resume">My Resume</div></a>
+            <a href="https://drive.google.com/file/d/1MBtASuU-Hqto375mea3ccLkTxaPD_6vO/view?usp=drive_link"><div className="hero-resume">My Resume</div></a>
         </div>
     </div>
   ) 
